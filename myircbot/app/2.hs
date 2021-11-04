cedb86a331a186f56f1b4d0a87d03707ba0ab54b
bilalbg <bilal.baig316@gmail.com>
2021-11-03 21:53:23 -0400

    Starting IRC bot

---
+++ myircbot/app/2.hs
@@ -0,0 +1,9 @@
+import System.IO
+import qualified Network.Socket as N 
+
+
+myServer = "irc.freenode.org"::String
+myPort = 6667 :: N.PortNumber
+myChan = "#tutbot-testing" :: String
+myNick = "tutbot" :: String 
+
