745feba4c696c7dca277820dab4ebe96ea9c0381
bilalbg <bilal.baig316@gmail.com>
2021-11-08 19:36:38 -0500

    another problem

---
+++ HackerRank/GradingStudents.hs
@@ -0,0 +1,11 @@
+module HackerRank.GradingStudents where
+
+main :: IO ()
+
+round5:: Int -> Int 
+round5 x 
+    | x >= 38 && (m5 - x) < 3 = m5 
+    | otherwise = x
+    where m5 = x + (5 - x `mod` 5 )
+
+main = interact $ unlines . map (show . round5 . read) . tail . words
